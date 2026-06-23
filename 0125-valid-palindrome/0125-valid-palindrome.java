4e2d3d75d18c5eb2905cd4191d449f42b7ba0810
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-06-23 12:14:02 +0530

    Time: 2 ms (99.2%), Space: 43.8 MB (98.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 1) {
+            return true;
+        }
+
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start < end) {
+            char c1 = s.charAt(start);
+            char c2 = s.charAt(end);
+            if(!Character.isLetterOrDigit(c1)) {
+                start++;
+            } else if(!Character.isLetterOrDigit(c2)) {
+                end--;
+            } else {
+                if(Character.toLowerCase(c1) != Character.toLowerCase(c2)) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+}

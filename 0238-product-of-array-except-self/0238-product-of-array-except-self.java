8eafe82f2de430699044870bc597946715762154
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-07-02 01:36:31 +0530

    Time: 7 ms (3.16%), Space: 65 MB (75.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,20 +3,18 @@ class Solution {
         int n = nums.length;
         int[] fwd = new int[n];
         int[] bwd = new int[n];
+        int[] res = new int[n];
         fwd[0] = 1;
         bwd[n-1] = 1;
 
         for(int i=1; i<n; i++) {
             fwd[i] = fwd[i-1]*nums[i-1];
             bwd[n-i-1] = bwd[n-i]*nums[n-i];
+
+            res[i] = bwd[i]*fwd[i];
+            res[n-i-1] = bwd[n-i-1]*fwd[n-i-1];
         }
 
-        int[] result = new int[n];
-
-        for(int i=0; i<n; i++) {
-            result[i] = fwd[i]*bwd[i];
-        }
-
-        return result;
+        return res;
     }
 }

ef95dcd6b61a28ca2c6d1c07fe60128e53ddd189
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-07-15 21:51:28 +0530

    Time: 2 ms (91.2%), Space: 44.4 MB (25%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,7 +2,6 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
-        int[][] time = new int[m][n];
         int maxTime = 0;
 
         for(int i=0; i<m; i++) {
@@ -16,29 +15,31 @@ class Solution {
         while(!q.isEmpty()) {
             int[] curr = q.poll();
 
-            if(curr[0] > 0 && grid[curr[0]-1][curr[1]] == 1 && time[curr[0]-1][curr[1]] == 0) {
-                time[curr[0]-1][curr[1]] = time[curr[0]][curr[1]] + 1;
+            if(curr[0] > 0 && grid[curr[0]-1][curr[1]] == 1) {
+                grid[curr[0]-1][curr[1]] = grid[curr[0]][curr[1]]+1;
+                maxTime = Math.max(maxTime, grid[curr[0]-1][curr[1]]-2);
                 q.offer(new int[]{curr[0]-1,curr[1]});
             }
-            if(curr[1] > 0 && grid[curr[0]][curr[1]-1] == 1 && time[curr[0]][curr[1]-1] == 0) {
-                time[curr[0]][curr[1]-1] = time[curr[0]][curr[1]] + 1;
+            if(curr[1] > 0 && grid[curr[0]][curr[1]-1] == 1) {
+                grid[curr[0]][curr[1]-1] = grid[curr[0]][curr[1]]+1;
+                maxTime = Math.max(maxTime, grid[curr[0]][curr[1]-1]-2);
                 q.offer(new int[]{curr[0],curr[1]-1});
             }
-            if(curr[0]+1 < m && grid[curr[0]+1][curr[1]] == 1 && time[curr[0]+1][curr[1]] == 0) {
-                time[curr[0]+1][curr[1]] = time[curr[0]][curr[1]] + 1;
+            if(curr[0]+1 < m && grid[curr[0]+1][curr[1]] == 1) {
+                grid[curr[0]+1][curr[1]] = grid[curr[0]][curr[1]]+1;
+                maxTime = Math.max(maxTime, grid[curr[0]+1][curr[1]]-2);
                 q.offer(new int[]{curr[0]+1,curr[1]});
             }
-            if(curr[1]+1 < n && grid[curr[0]][curr[1]+1] == 1 && time[curr[0]][curr[1]+1] == 0) {
-                time[curr[0]][curr[1]+1] = time[curr[0]][curr[1]] + 1;
+            if(curr[1]+1 < n && grid[curr[0]][curr[1]+1] == 1) {
+                grid[curr[0]][curr[1]+1] = grid[curr[0]][curr[1]]+1;
+                maxTime = Math.max(maxTime, grid[curr[0]][curr[1]+1]-2);
                 q.offer(new int[]{curr[0],curr[1]+1});
             }
         }
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(grid[i][j] == 1 && time[i][j] > 0) {
-                    maxTime = Math.max(maxTime, time[i][j]);
-                } else if(grid[i][j] == 1 && time[i][j] == 0) {
+                if(grid[i][j] == 1) {
                     return -1;
                 }
             }

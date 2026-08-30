024ee1b7d744c711f5a37caa8784a4f917a61d99
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-08-31 00:11:16 +0530

    Time: 8 ms (1.86%), Space: 44.2 MB (5.62%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,31 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Map<Character,Deque<Integer>> mp = new HashMap<>();
+        Map<Character,List<Integer>> mp = new HashMap<>();
 
         for(int i=0; i<t.length(); i++) {
-            mp.putIfAbsent(t.charAt(i), new ArrayDeque<>());
-            mp.get(t.charAt(i)).offerLast(i);
+            mp.putIfAbsent(t.charAt(i), new ArrayList<>());
+            mp.get(t.charAt(i)).add(i);
         }
 
         int last = -1;
 
         for(int i=0; i<s.length(); i++) {
             if(mp.containsKey(s.charAt(i))) {
-                Deque<Integer> curr = mp.get(s.charAt(i));
+                List<Integer> curr = mp.get(s.charAt(i));
                 boolean change = false;
-                while(!curr.isEmpty()) {
-                    int idx = curr.pollFirst();
-                    if(idx > last) {
-                        last = idx;
+                int min = 0, mid = 0, max = curr.size()-1;
+                
+                while(min <= max) {
+                    mid = min+(max-min)/2;
+
+                    if(mid > 0 && curr.get(mid-1) > last) {
+                        max = mid-1;
+                    } else if(curr.get(mid) <= last) {
+                        min = mid+1;
+                    } else {
+                        last = curr.get(mid);
                         change = true;
+                        System.out.println(s.charAt(i)+":"+last);
                         break;
                     }
                 }

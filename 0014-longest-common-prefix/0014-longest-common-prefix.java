402ac6df27d1c882b839a02c42d1da648fbc294e
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-08-31 01:00:44 +0530

    Time: 1 ms (60.71%), Space: 43.3 MB (50.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,19 +3,8 @@ class Solution {
         StringBuilder res = new StringBuilder("");
 
         for(int i=0;;i++) {
-            boolean exit = false;
-
-            for(String s : strs) {
-                if(i==s.length() || s.charAt(i) != strs[0].charAt(i)) {
-                    exit = true;
-                    break;
-                }
-            }
-
-            if(exit) break;
-            else res.append(strs[0].charAt(i));
+            for(String s : strs) if(i==s.length() || s.charAt(i) != strs[0].charAt(i)) return res.toString();
+            res.append(strs[0].charAt(i));
         }
-
-        return res.toString();
     }
 }

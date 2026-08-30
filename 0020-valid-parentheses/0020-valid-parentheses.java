8d505542189aeff841a801f9bfffbd3a7b7f027f
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-08-30 16:44:52 +0530

    Time: 3 ms (86.08%), Space: 43.4 MB (25.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,10 +4,9 @@ class Solution {
 
         for(char curr : s.toCharArray()) {
             if(curr == '(' || curr == '{' || curr == '[') stack.push(curr);
-            else if(stack.isEmpty()) return false;
-            else if(curr == ')' && stack.pop() != '(') return false;
-            else if(curr == '}' && stack.pop() != '{') return false;
-            else if(curr == ']' && stack.pop() != '[') return false;
+            else if(curr == ')' && (stack.isEmpty() || stack.pop() != '(')) return false;
+            else if(curr == '}' && (stack.isEmpty() || stack.pop() != '{')) return false;
+            else if(curr == ']' && (stack.isEmpty() || stack.pop() != '[')) return false;
         }
 
         return stack.isEmpty();

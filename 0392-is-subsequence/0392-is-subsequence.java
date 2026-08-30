7028b0634afe01f871fbf174b86fa95ec3d8eab7
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-08-31 00:22:05 +0530

    Time: 5 ms (1.86%), Space: 44.1 MB (5.62%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,37 +1,38 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        Map<Character,List<Integer>> mp = new HashMap<>();
+    private Map<Character,List<Integer>> mp;
+
+    private void preprocess(String t) {
+        mp = new HashMap<>();
 
         for(int i=0; i<t.length(); i++) {
             mp.putIfAbsent(t.charAt(i), new ArrayList<>());
             mp.get(t.charAt(i)).add(i);
         }
+    }
+
+    private int searchIndex(List<Integer> list, int last) {
+        int min = 0, mid = 0, max = list.size()-1;
+                
+        while(min <= max) {
+            mid = min+(max-min)/2;
+            if(mid > 0 && list.get(mid-1) > last) {
+                max = mid-1;
+            } else if(list.get(mid) <= last) {
+                min = mid+1;
+            } else return list.get(mid);
+        }
+
+        return -1;
+    }
+
+    public boolean isSubsequence(String s, String t) {
+        preprocess(t);
 
         int last = -1;
-
         for(int i=0; i<s.length(); i++) {
             if(mp.containsKey(s.charAt(i))) {
-                List<Integer> curr = mp.get(s.charAt(i));
-                boolean change = false;
-                int min = 0, mid = 0, max = curr.size()-1;
-                
-                while(min <= max) {
-                    mid = min+(max-min)/2;
-
-                    if(mid > 0 && curr.get(mid-1) > last) {
-                        max = mid-1;
-                    } else if(curr.get(mid) <= last) {
-                        min = mid+1;
-                    } else {
-                        last = curr.get(mid);
-                        change = true;
-                        System.out.println(s.charAt(i)+":"+last);
-                        break;
-                    }
-                }
-
-                if(!change) return false;
-
+                last = searchIndex(mp.get(s.charAt(i)), last);
+                if(last == -1) return false;
             } else return false;
         }
 

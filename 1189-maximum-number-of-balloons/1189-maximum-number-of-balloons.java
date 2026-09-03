167c4fe0dff70744df89becfc63f06af71cf98c4
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-03 14:13:53 +0530

    Time: 2 ms (94.26%), Space: 43.2 MB (75.57%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,22 +1,14 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Set<Character> charSet =  new HashSet<>();
-        Map<Character,Integer> charMap = new HashMap<>();
+        int[] chars = new int[26];
+
+        for(char c : text.toCharArray()) chars[c-'a']++;
+
+        int res = Integer.MAX_VALUE;
 
         for(char c : "balon".toCharArray()) {
-            charMap.put(c,0);
-            charSet.add(c);
-        }
-        
-        for(char c : text.toCharArray()) if(charSet.contains(c)) charMap.merge(c,1,Integer::sum);
-
-        Iterator<Map.Entry<Character,Integer>> itr = charMap.entrySet().iterator();
-        int res = Integer.MAX_VALUE;
-        
-        while(itr.hasNext()) {
-            Map.Entry<Character,Integer> curr = itr.next();
-            if(curr.getKey() == 'l' || curr.getKey() == 'o') res = Math.min(res,curr.getValue()/2);
-            else res = Math.min(res,curr.getValue());
+            if(c == 'l' || c == 'o') res = Math.min(res,chars[c-'a']/2);
+            else res = Math.min(res,chars[c-'a']);
         }
 
         return res;

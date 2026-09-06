0b2a5616c4a2df8bf4fb68bd948da41acb6789b3
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-06 16:10:30 +0530

    Time: 918 ms (5.95%), Space: 63.3 MB (5.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,45 +1,53 @@
 class Solution {
-
-    private int findIdx(int[]nums, int n, int start, int end) {
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-
-            if(nums[mid] == n) {
-                return mid;
-            } else if(nums[mid] < n) {
-                start = mid+1;
-            } else {
-                end = mid-1;
-            }
-        }
-
-        return -1;
+    private String toString(List<Integer> nums) {
+        return nums.get(0)+","+nums.get(1)+","+nums.get(2);
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int len = nums.length;
-        int left = 0;
-        int right = len-1;
+        Map<Integer,Integer> s = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
 
-        Set<List<Integer>> result = new HashSet<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        while(left < len-2) {
-            int val = (nums[left]+nums[right])*(-1);
-            int pos = findIdx(nums,val,left+1,right-1);
-
-            if(pos != -1) {
-                result.add(new ArrayList<>(List.of(nums[left],nums[pos],nums[right])));
-            }
-
-            if(right-left == 1) {
-                left++;
-                right = len;
-            }
-
-            right--;
+        for(int n : nums) {
+            s.put(n, s.getOrDefault(n,0)+1);
+            
+            min = Math.min(min,n);
+            max = Math.max(max,n);
         }
 
-        return new ArrayList<>(result);
+        if(min > 0 || max < 0) return res;
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i] >= 0) break;
+            
+            for(int j=nums.length-1; j>i; j--) {
+                
+                int val = (-1)*(nums[i]+nums[j]);
+                int n = 1;
+
+                if(val == nums[i]) n++;
+                if(val == nums[j]) n++;
+
+                if(s.getOrDefault(val,0) >= n) {
+                    List<Integer> curr = new ArrayList<>(List.of(nums[i],val,nums[j]));
+                    Collections.sort(curr);
+                    String str = toString(curr);
+                    
+                    if(!visited.contains(str)) {
+                        res.add(curr);
+                        visited.add(str);
+                    }
+                }
+            }
+        }
+
+        if(s.getOrDefault(0,0) >= 3) res.add(List.of(0,0,0));
+
+        return res;
     }
 }

4e5a906278b72f4cae99cf2e52ac38701e3c82c9
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-06 17:39:35 +0530

    Time: 30 ms (87.48%), Space: 58.9 MB (85.97%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,52 +1,33 @@
 class Solution {
-    private String toString(int a, int b) {
-        return a+","+b;
-    }
-
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer,Integer> s = new HashMap<>();
-        List<List<Integer>> res = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for(int n : nums) {
-            s.put(n, s.getOrDefault(n,0)+1);
-            
-            min = Math.min(min,n);
-            max = Math.max(max,n);
-        }
-
-        if(min > 0 || max < 0) return res;
-
         Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-1; i++) {
-            if(nums[i] >= 0) break;
-            
-            for(int j=nums.length-1; j>i; j--) {
-                if(nums[j] <= 0) break;
+        List<List<Integer>> res = new ArrayList<>();
 
-                int val = (-1)*(nums[i]+nums[j]);
-                int dups = 1;
-                
-                if(val > nums[j] || val < nums[i]) continue;
+        for(int i=0; i<nums.length-2; i++) {
+            int target = -(nums[i]);
 
-                if(val == nums[i] || val == nums[j]) dups = 2;
+            if(nums[i] > 0) break; 
 
-                if(s.getOrDefault(val,0) >= dups) {
-                    String str = toString(nums[i],nums[j]);
-                    
-                    if(!visited.contains(str)) {
-                        res.add(List.of(nums[i],val,nums[j]));
-                        visited.add(str);
-                    }
-                }
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int m = i+1;
+            int n = nums.length-1;
+
+            while(m<n) {
+                int val = nums[m]+nums[n];
+                if(val == target) {
+                    res.add(List.of(nums[i],nums[m],nums[n]));
+
+                    m++;
+                    n--;
+
+                    while(m < n && nums[m] == nums[m-1]) m++;
+                    while(n > m && nums[n] == nums[n+1]) n--;
+                } else if(val > target) n--;
+                else m++;
             }
         }
-
-        if(s.getOrDefault(0,0) >= 3) res.add(List.of(0,0,0));
 
         return res;
     }

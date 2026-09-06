279fe499553b6116bc914fc6de883aa886a84024
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-07 01:41:32 +0530

    Time: 23 ms (19.31%), Space: 53.6 MB (81.36%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,15 +1,12 @@
 class Solution {
-    private int getDistincts(String s, String t, int i, int j, int[][] dp) {
+    private int getDistincts(String s, String t, int lenS, int lenT, int i, int j, int[][] dp) {
+        if(j == lenT) return 1;
+        if(i == lenS) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int val = 0;
+        int val = getDistincts(s,t,lenS,lenT,i+1,j,dp);
 
-        for(int x=i; x<=s.length()-t.length()+j; x++) {
-            if(s.charAt(x) == t.charAt(j)) {
-                if(j+1 == t.length()) val++;
-                else if(x+1 != s.length()) val += getDistincts(s,t,x+1,j+1,dp);
-            }
-        }
+        if(s.charAt(i) == t.charAt(j)) val += getDistincts(s,t,lenS,lenT,i+1,j+1,dp);
 
         dp[i][j] = val;
 
@@ -17,12 +14,14 @@ class Solution {
     }
 
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()][t.length()];
+        int lenS = s.length(), lenT = t.length();
+        int[][] dp = new int[lenS][lenT];
 
-        for(int i=0; i<s.length(); i++) {
+
+        for(int i=0; i<lenS; i++) {
             Arrays.fill(dp[i],-1);
         }
 
-        return getDistincts(s,t,0,0,dp);
+        return getDistincts(s,t,lenS,lenT,0,0,dp);
     }
 }

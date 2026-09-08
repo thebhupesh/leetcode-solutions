d95bf0d1f5d43d8983345ce1e53cfaa8b12dc46c
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-09 03:03:48 +0530

    Time: 25 ms (32.44%), Space: 46.5 MB (44.4%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,24 @@
 class Solution {
-    private int evaluate(int[] coins, int amount, int pos, int[][] dp) {
-        if(dp[amount][pos] != -2) return dp[amount][pos];
+    private int evaluate(int[] coins, int amount, int[] dp) {
+        if(dp[amount] != -2) return dp[amount];
         if(amount == 0) return 0;
 
         int count = Integer.MAX_VALUE;
-        int invalid = 0;
-
-        for(int i=pos; i>=0; i--) {
-            if(amount >= coins[i]) {
-                int val = evaluate(coins,amount-coins[i],i,dp);
-
+        for(int coin : coins) {
+            if(amount >= coin) {
+                int val = evaluate(coins,amount-coin,dp);
                 if(val != -1) count = Math.min(count,val+1);
-                else invalid++;
-            } else invalid++;
+            }
         }
 
-        dp[amount][pos] = (invalid-1 == pos) ? -1 : count;
-
-        return dp[amount][pos];
+        dp[amount] = (count == Integer.MAX_VALUE) ? -1 : count;
+        return dp[amount];
     }
 
-    public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
-        
-        int[][] dp = new int[amount+1][coins.length];
-        
-        for(int[] arr : dp) Arrays.fill(arr,-2);
+    public int coinChange(int[] coins, int amount) {        
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,-2);
 
-        return evaluate(coins,amount,coins.length-1,dp);
+        return evaluate(coins,amount,dp);
     }
 }

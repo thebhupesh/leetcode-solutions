c6f03169f69b30d1fd2ecb27c267df226b549332
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-10 02:01:40 +0530

    Time: 1 ms (99.24%), Space: 42.4 MB (87.07%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,23 +1,14 @@
 class Solution {
-    private long findValue(long num, long div, int multiplier) {
-        return (((num/div) - 1)*div + (num%div) + 1)*multiplier;
+    private long findValue(long num, long div) {
+        return ((num/div) - 1)*div + (num%div) + 1;
     }
 
     public long countCommas(long n) {
-        if(n < 1_000) return 0;
-        
-        long val = 0;
-
-        if(n > 999_999) val += 999_000;
-        else return findValue(n,1_000,1);
-
-        if(n > 999_999_999) val += 999_000_000*2;
-        else return val + findValue(n,1_000_000,2);
-
-        if(n > 999_999_999_999L) val += 999_000_000_000L*3;
-        else return val + findValue(n,1_000_000_000L,3);
-
-        if(n > 999_999_999_999_999L) return val + 999_000_000_000_000L*4+5;
-        else return val + findValue(n,1_000_000_000_000L,4);
+        if(n <= 999) return 0;
+        else if(n <= 999_999) return findValue(n,1_000);
+        else if(n <= 999_999_999) return 999_000L + findValue(n,1_000_000)*2;
+        else if(n <= 999_999_999_999L) return 999_000L + 999_000_000L*2 + findValue(n,1_000_000_000L)*3;
+        else if(n <= 999_999_999_999_999L) return 999_000L + 999_000_000L*2 + 999_000_000_000L*3 + findValue(n,1_000_000_000_000L)*4;
+        else return 3998998998999005L;
     }
 }

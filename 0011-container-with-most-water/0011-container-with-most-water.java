cdc20ce5fc3b05c84c09050ee6e957af24190b76
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-10 02:35:06 +0530

    Time: 2 ms (99.61%), Space: 77.6 MB (12.75%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -6,10 +6,14 @@ class Solution {
         int res = 0;
 
         while(p1 < p2) {
-            res = Math.max(res,(p2-p1)*Math.min(height[p1],height[p2]));
+            int low = Math.min(height[p1],height[p2]);
+            res = Math.max(res,(p2-p1)*low);
 
             if(height[p1] <= height[p2]) p1++;
             else p2--;
+
+            while(p1<p2 && height[p1] <= low) p1++;
+            while(p1<p2 && height[p2] <= low) p2--;
         }
 
         return res;

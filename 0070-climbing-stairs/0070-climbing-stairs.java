f001597845d0fe8699f858d62e1ff89876c08599
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-11 00:38:48 +0530

    Time: 0 ms (100%), Space: 42 MB (72.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,12 @@
 class Solution {
-    int[] dp = new int[46];
-    private int findCount(int n, int curr) {
-        if(curr > n) {
-            return 0;
-        }
-        if(dp[n-curr] != -1) {
-            return dp[n-curr];
-        }
-        if(curr == n) {
-            return 1;
-        }
-
-        dp[n-curr] = findCount(n,curr+1)+findCount(n,curr+2);
-
-        return dp[n-curr];
+    private int climb(int n, int[] dp) {
+        if(n == -1) return 0;
+        else if(n == 0) return 1;
+        else if(dp[n] != 0) return dp[n];
+        return dp[n] = climb(n-1,dp)+climb(n-2,dp);
     }
 
     public int climbStairs(int n) {
-        Arrays.fill(dp,-1);
-        return findCount(n,0);
+        return climb(n,new int[n+1]);
     }
 }

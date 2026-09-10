55b23aee5dd4c96d7c0461f0fc0940353d414fe3
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-10 23:26:11 +0530

    Time: 22 ms (96.05%), Space: 103.6 MB (88.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,24 +1,15 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] res = new int[temperatures.length];
-        List<List<Integer>> temps = new ArrayList<>();
-
-        int min = temperatures[0];
-        
-        for(int i=0; i<71; i++) temps.add(new ArrayList<>());
+        Deque<Integer> s = new ArrayDeque<>();
 
         for(int i=0; i<temperatures.length; i++) {
-            for(int j=temperatures[i]-1; j>=min; j--) {
-                List<Integer> temp = temps.get(j-30);
-                for(int k=temp.size()-1; k>=0; k--) {
-                    if(res[temp.get(k)] == 0) {
-                        res[temp.get(k)] = i-temp.get(k);
-                        temp.remove(k);
-                    }
-                }
+            while(!s.isEmpty() && temperatures[s.peek()] < temperatures[i]) {
+                int idx = s.pop();
+                res[idx] = i-idx;
             }
-            temps.get(temperatures[i]-30).add(i);
-            min = Math.min(temperatures[i],min);
+
+            s.push(i);
         }
 
         return res;

05805e777f780c6d6e2514db463c7acaf4650816
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-15 00:38:18 +0530

    Time: 0 ms (100%), Space: 42.7 MB (52.92%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -3,11 +3,10 @@ class Solution {
         
         // if(((rec1[0] >= rec2[0] && rec1[0] < rec2[2]) || (rec1[2] > rec2[0] && rec1[2] < rec2[2]) || (rec2[0] >= rec1[0] && rec2[0] < rec1[2]) || (rec2[2] > rec1[0] && rec2[2] < rec1[2])) && ((rec1[1] >= rec2[1] && rec1[1] < rec2[3]) || (rec1[3] > rec2[1] && rec1[3] < rec2[3]) || (rec2[1] >= rec1[1] && rec2[1] < rec1[3]) || (rec2[3] > rec1[1] && rec2[3] < rec1[3]))) return true;
 
-        if(rec1[0] > rec2[2] || rec2[0] >= rec1[2] || rec1[1] > rec2[3] || rec2[1] >= rec1[3]) return false;
+        // return false;
+
+        if(rec1[0] >= rec2[2] || rec2[0] >= rec1[2] || rec1[1] >= rec2[3] || rec2[1] >= rec1[3]) return false;
 
         return true;
-        
-
-        // return false;
     }
 }

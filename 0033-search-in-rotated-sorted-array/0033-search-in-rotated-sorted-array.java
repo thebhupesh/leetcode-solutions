e5ae7f2804ae1e219d73786106270c85699b3d44
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-19 01:38:37 +0530

    Time: 0 ms (100%), Space: 44 MB (28.3%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int l = 0, r = n-1;
-        int k = n-1;
-        
-        while(l<=r) {
-            int m = l + (r-l)/2;
+        int min = 0;
+        int max = nums.length-1;
 
-            if(nums[m] >= nums[(n+m-1)%n] && nums[m] >= nums[(m+1)%n]) {
-                k = m;
-                break;
-            } else if(nums[m] > nums[n-1]) {
-                l = m+1;
-            } else if(nums[m] < nums[n-1]) {
-                r = m-1;
-            }
+        while(min<max) {
+            int mid = min + (max-min)/2;
+
+            if(nums[mid] > nums[nums.length-1]) min = mid+1;
+            else max = mid;
         }
 
-        if(target >= nums[0]) {
-            l = 0;
-            r = k;
+        if(target <= nums[nums.length-1]) {
+            min = max;
+            max = nums.length-1;
         } else {
-            l = k+1;
-            r = n-1;
+            min = 0;
+            max -= 1;
         }
 
-        while(l<=r) {
-            int m = l + (r-l)/2;
+        while(min<=max) {
+            int mid = min + (max-min)/2;
 
-            if(nums[m] == target) return m;
-            else if(nums[m] < target) l = m+1;
-            else r = m-1;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) min = mid+1;
+            else max = mid-1;
         }
 
         return -1;

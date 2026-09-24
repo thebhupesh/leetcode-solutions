240f9b409a2c050a8b6907c406db5ff6e79dcacb
Bhupesh Pandey <81607715+thebhupesh@users.noreply.github.com>
2026-09-25 00:04:36 +0530

    Time: 1 ms (99.69%), Space: 45.4 MB (89.71%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,12 +1,17 @@
 class Solution {
-    private boolean check(String num, int i) {
+    private boolean check(int num, int i) {
         int sum = 0;
-        for(char digit : num.toCharArray()) sum += (digit-'0');
+        
+        while(num != 0) {
+            sum += num%10;
+            num /= 10;
+        }
+        
         return sum == i;
     }
 
     public int smallestIndex(int[] nums) {
-        for(int i=0; i<nums.length; i++) if(check(Integer.toString(nums[i]),i)) return i;
+        for(int i=0; i<nums.length; i++) if(check(nums[i],i)) return i;
         return -1;
     }
 }
